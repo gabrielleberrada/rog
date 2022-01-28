85dfd9e57ac932626e05842157f01475e7cdf9c0
21deferri <pierre.de_ferrieres_de_sauveboeuf@mines-paristech.fr>
2022-01-28 12:32:45 +0100

    pdf matin

--- rog.py
+++ rog.py
@@ -16,6 +16,48 @@ PIXEL_SIZE = 20
 def draw_char(text, position, font, color=BLACK, background=WHITE):
     return font.render(text, False, color, background), position
 
+## initialisation du sac
+sac = {'taille' : 0, 'vie': 10, 'portefeuille' : 0}
+taille_max = 5
+
+## Classes Objets et monstres
+class Objet :
+    """prend un objet avec un nom (str affiché sur la map), un type (arme, armure...)
+    """
+    def __init__(self, name):
+        self.name = name #un array d'une lettre
+         #On rajoute un num ????
+
+    def take(self, Prendre = True) : #Pour l'instant on prend les pièces une par une à voir comment on fait pour le nombre
+        pris = False
+        if sac['taille'] >= taille_max :
+            pass
+        if self.name == '*':
+            sac['portefeuille']+=1
+            mat_obj[x_position, y_position] = None
+            #Modification de la matrice objet
+            new_obj = np.random.choice(names_obj) 
+            i,j,state = np.random.choice(DOTS) 
+            while state == 1 : 
+                i,j,state = np.random.choice(DOTS)
+            mat_obj[i,j] = new_obj 
+            return sac
+        caption = "Prendre l'objet Y/N?" #voir comment il répond et comment interagir
+        if Prendre == True :
+            if self.name == 'j': #potion magique qui remet la vie à 10 points
+                sac['vie'] = 10
+            elif self.name in sac :            
+                sac[self.name] += 1
+                sac['taille']+= 1
+            else : 
+                sac[self.name] = 1
+            mat_obj[x_position, y_position] = None
+            new_obj = np.random.choice(names_obj) # on choisit un objet au hasard
+            i,j,state = np.random.choice(DOTS) #on choisit un triplet dans la liste de Tonio
+            while state == 1 : #si la place est déjà occupée
+                i,j,state = np.random.choice(DOTS)
+            mat_obj[i,j] = new_obj 
+
 ## initialisation de la MAP et de DOTS
 
 MAP = np.full((25,25), None)
@@ -78,7 +120,18 @@ for (i, j) in product(range(21), range(21)):
     if MAP[i, j] == '.':
         DOTS.append((i, j , 0))
 
-        
+#Création de la matrice des objets
+mat_obj = np.full((25,25), None)
+names_obj = ['*','j','!','(','&','o','n']
+
+for i in range(10) :
+    new_obj = np.random.choice(names_obj) # on choisit un objet au hasard
+    i,j,state = np.random.choice(DOTS) #on choisit un triplet dans la liste de Tonio
+    while state == 1 : #si la place est déjà occupée
+         i,j,state = np.random.choice(DOTS)
+    mat_obj[i,j] = new_obj  
+
+
 ## initialisation de la fenêtre
 
 pg.init()
